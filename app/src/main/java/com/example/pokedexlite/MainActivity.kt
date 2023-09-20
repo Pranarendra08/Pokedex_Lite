d5d062cbd02620418ccb6681aec62cbe733792bf
Pranarendra08 <pranarendra@gmail.com>
2023-09-20 22:02:43 +0700

    setup layout

--- app/src/main/java/com/example/pokedexlite/MainActivity.kt
+++ app/src/main/java/com/example/pokedexlite/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.pokedexlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.pokedexlite.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_favorite
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        supportActionBar?.apply {
+            setTitle(R.string.pokedex)
+        }
+        navView.setupWithNavController(navController)
     }
 }
